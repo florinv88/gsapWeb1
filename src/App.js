bf2cf8077a6341873a8a20d5dd5004de58d86842
florinv88 <florinvisan022@gmail.com>
2022-12-25 11:34:21 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import gsap from 'gsap';
+
+import Banner from "./components/Banner";
+import Cases from "./components/Cases";
+import Header from "./components/Header";
+import IntroOverlay from "./components/IntroOverlay";
 
 function App() {
+
+
+
+  useEffect(() => {
+
+    // UseEffect : to don't have any errors on mobile devices 
+    // the vh on mobile devices includes the menu bar / the link from the top etcc
+    // see more here about it : https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
+
+    let vh = window.innerHeight * .01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`)
+
+    //prevents the flashing
+    gsap.to("body", { css: { visibility: "visible" }, duration: 0 })
+
+    //timeline
+    const tl = gsap.timeline();
+
+    tl.fromTo('.line span', {
+      y: 100,
+      skewY: 7,
+      ease: "power4.out",
+    }, {
+      y: 0,
+      duration: 2,
+      delay: 1,
+      skewY: 0,
+      stagger: {
+        amount: 0.3   //the delay for the second item who is a span in .line class
+      }
+
+    })
+
+    tl.to('.overlay-top', {
+      height: 0,
+      ease: 'expo.inOut',
+      stagger: 0.4,
+      duration: 1.6
+    })
+
+    tl.to('.overlay-bottom', {
+      width: 0,
+      ease: 'expo.inOut',
+      duration: 1.6,
+      delay: -0.8,
+      stagger: 0.4
+    })
+
+    tl.to('.intro-overlay', {
+      css: { display: "none" }
+    })
+
+
+
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
+      <IntroOverlay />
+      <Header />
+      <Banner />
+      <Cases />
     </div>
   );
 }
